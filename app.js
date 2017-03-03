273bbe1d9aed86a6ed8ad6cdc6bcbf3893d0b775
Nick D'hondt <nick.dhondt@student.howest.be>
2017-03-03 15:47:23 +0100

    Update

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
 var sectionsList = [["#helloSection", "#helloLink"], ["#skillsSection", "#skillsLink"], ["#showcaseSection", "#showcaseLink"], ["#contactSection", "#contactLink"]];
 var deltaY = 0;
-var prevSection = 1;
+var prevSection = 0;
+var mq = window.matchMedia("(min-width: 768px)");
 
-$(document).mousewheel(function(event) {
-    deltaY += event.deltaY;
+// $(document).mousewheel(function(event) {
+//     if(deltaY < 0 && event.deltaY > 0) deltaY = 0;
+//     else if (deltaY > 0 && event.deltaY < 0) deltaY = 0;
+//     deltaY += event.deltaY;
+//
+//     if (prevSection <= sectionsList.length) {
+//         if (deltaY < -3 && sectionsList[prevSection + 1] !== undefined) {
+//             $(sectionsList[prevSection + 1][1]).click();
+//             deltaY = 0;
+//         }
+//
+//         if (deltaY > 3 && sectionsList[prevSection - 1] !== undefined) {
+//             $(sectionsList[prevSection - 1][1]).click();
+//             deltaY = 0;
+//         }
+//     }
+// });
 
-    var prevSectionCopy = prevSection;
+if (mq.matches) {
 
-    if (prevSection <= sectionsList.length) {
-        if (deltaY < -3) {
-            $(sectionsList[prevSectionCopy][1]).click();
-            deltaY = 0;
-        }
-
-        if (deltaY > 3) {
-            $(sectionsList[prevSectionCopy - 2][1]).click();
-            deltaY = 0;
-        }
-    }
-});
+}
 
 sectionsList.forEach(function (linkData, index) {
-    var altIndex = index;
-    index++;
     $(linkData[1]).click(function () {
         prevSection = index;
 
-        $("#indicator").css("left", (index) * 20 + "%");
+        $("#indicator").css("left", (index + 1) * 20 + "%");
         $("#indicator-line").css("width", $(this).children().first().width() + "px");
-
-
-        $("#sectionHandle").css("left", altIndex * -100 + "%");
+        $("#sectionHandle").css("left", index * -100 + "%");
     })
 });
